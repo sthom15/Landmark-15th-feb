170ed0b9657eea8c0809c78509bc607ad383d96a
sheba <sheba.s.thomas@student.uts.edu.au>
2023-02-15 10:05:39 +1100

    Initial Commit

---
+++ Landmark 15th feb/Landmark_15th_febApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_15th_febApp.swift
+//  Landmark 15th feb
+//
+//  Created by Sheba Thomas  on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_15th_febApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
